cb30fb34430b2bec349b9f4f1708d515101663bc
Gracie <battlesbrown@gmail.com>
2025-03-10 13:46:50 -0500

    Framework

---
+++ main.js
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------------------------
+//                                       Pet Game
+//                                  By Gracie Hutchins
+//------------------------------------------------------------------------------------------------
+//--------------------------------------Variables-------------------------------------------------
+// Keeps track of how much sleep pet needs
+let sleepScore = 0;
+
+// Keeps track of how hungry pet is
+let hungryScore = 0;
+
+// Keeps track of how much the pet needs to be entertained 
+let playScore = 0;
+
+//--------------------------------------Functions-------------------------------------------------
+// Puts pet to sleep -- +restores sleep
+function sleep(){
+    
+}
+
+// Feeds pet -- +restores pet hunger
+function eat(){
+
+}
+
+// Plays with pet -- +restores pets entertainment 
+function play(){
+
+}
