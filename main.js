cb309b9b8c8660b3a82b94b6473645ccac2c41ef
Gracie <battlesbrown@gmail.com>
2025-03-10 14:27:25 -0500

    Create html, buttons, unit checks and stats

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 //                                       Pet Game
 //                                  By Gracie Hutchins
 //------------------------------------------------------------------------------------------------
+// Unit checks
+window.onload = function() {
+    console.log("HTML/CSS LOADED"); // Page is fully loaded
+};
+
 //--------------------------------------Variables-------------------------------------------------
 // Keeps track of how much sleep pet needs
 let sleepScore = 0;
@@ -13,17 +18,46 @@ let hungryScore = 0;
 let playScore = 0;
 
 //--------------------------------------Functions-------------------------------------------------
+// Checks pet stats
+function stats(){
+    // Needs sleep
+    if (sleepScore <= 5){
+        alert("Your pet needs sleep!");
+    }
+
+    // Needs Food
+    if (hungryScore <= 5){
+        alert("Your pet needs food!");
+    }
+
+    // Needs play time
+    if (playScore <= 5){
+        alert("Your pet needs to play!");
+    }
+}
+
 // Puts pet to sleep -- +restores sleep
 function sleep(){
-    
+    sleepScore = 10;
+    stats();
 }
 
 // Feeds pet -- +restores pet hunger
 function eat(){
-
+    hungryScore = 10;
+    stats();
 }
 
 // Plays with pet -- +restores pets entertainment 
 function play(){
-
+    playScore = 10;
+    stats();
 }
+
+//---------------------------------------Player inputs-----------------------------------------------
+// 
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("sleep").addEventListener("click", sleep);
+    document.getElementById("eat").addEventListener("click", eat);
+    document.getElementById("play").addEventListener("click", play);
+});
